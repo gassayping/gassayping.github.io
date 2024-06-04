dafaa6f5d23a25b9cde589b2d4b8f4d8499bfe71
Gassayping <Dev_Gassayping@proton.me>
2024-06-03 18:17:26 -0700

    Added links to socials
    Gave links to github and mastodon

--- docs/script.js
+++ docs/script.js
@@ -4,24 +4,21 @@ let columns = rows = 0;
 const body = document.body,
 	html = document.documentElement;
 
-const height = Math.max(body.scrollHeight, body.offsetHeight,
-	html.clientHeight, html.scrollHeight, html.offsetHeight);
-wrapper.style.setProperty("--height", height + "px");
-
 createGrid();
 
 const text = document.getElementById("type-anim");
 setInterval(() => {
-	if (text.textContent == "dev ") {
-		text.textContent = "dev|";
-	} else {
-		text.textContent = "dev ";
-	}
+	if (text.textContent) text.textContent = "";
+	else text.textContent = "|";
 }, 750);
 
 window.onresize = createGrid;
 
 function createGrid() {
+	const height = Math.max(body.scrollHeight, body.offsetHeight,
+		html.clientHeight, html.scrollHeight, html.offsetHeight);
+	wrapper.style.setProperty("--height", height - 1 + "px");
+
 	wrapper.innerHTML = "";
 	columns = Math.floor(document.body.clientWidth / size);
 	rows = Math.floor(height / size);
